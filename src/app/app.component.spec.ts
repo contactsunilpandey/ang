91c85aa8fce10250b7582aba4bbbb9bb864c6a65
contactsunilpandey <40923585+contactsunilpandey@users.noreply.github.com>
2022-09-12 17:08:18 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ang app is not running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ang app is running!');
   });
 });
