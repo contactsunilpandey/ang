0f621a0537a5daacdb6a022cce2296b273c83999
contactsunilpandey <contact.sunil.pandey@gmail.com>
2022-09-15 16:18:18 +0530

    export inside config export

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,13 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
+
 
 module.exports = function (config) {
+
+  const puppeteer = require('puppeteer');
+  process.env.CHROME_BIN = puppeteer.executablePath();
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
